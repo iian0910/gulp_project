d75ebaf5c9d7aaf7529741d94cf22a0966d4040b
ZongSheng <fancyian05180910@gmail.com>
2020-03-19 16:22:24 +0800

    Update 2020-03-19T08:22:11.489Z

---
+++ js/all.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var newFunc = function newFunc() {
+  console.log('Aa');
+};
+
+newFunc();
+"use strict";
+
+var newFunc2 = function newFunc2() {
+  console.log('Bb');
+};
+
+newFunc2();
+//# sourceMappingURL=all.js.map
--- source/js/all.js
+++
@@ -1,4 +0,0 @@
-let newFunc = () => {
-	console.log('Aa')
-}
-newFunc()
