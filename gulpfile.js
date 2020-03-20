0dcfb9b3bce8bb16d036c5f8a04807c1dd66a91e
ZongSheng <fancyian05180910@gmail.com>
2020-03-20 11:57:54 +0800

    升級 gulp 4.x 版本

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ console.log(options)
 
 // 清空指定資料夾 指令：gulp clean
 gulp.task('clean', function () {
-	return gulp.src(['./.tmp', './public'], { read: false })
+	return gulp.src(['./.tmp', './public'], { read: false, allowEmpty: true })
 		.pipe($.clean())
 })
 
@@ -36,7 +36,10 @@ gulp.task('sass', function () {
 	return gulp.src('./source/scss/**/*.scss')
 		.pipe($.plumber())
 		.pipe($.sourcemaps.init())
-		.pipe($.sass().on('error', $.sass.logError))
+		.pipe($.sass({
+			outputStyle: 'nested',
+			includePaths: ['./node_modules/bootstrap/scss']
+		}).on('error', $.sass.logError))
 		// ↑↑ 已編譯完成 下一步準備輸出 ↑↑
 		.pipe($.postcss([autoprefixer()]))
 		// ↑↑ 輸出前先優化CSS ↑↑
@@ -66,24 +69,18 @@ gulp.task('babel', () =>
 
 // 套件管理 Bower
 gulp.task('bower', function () {
-	return gulp.src(mainBowerFiles({
-		"overrides": {
-			"bootstrap": {
-				"main": "dist/js/bootstrap.bundle.js"
-			},
-			"jquery": {
-				"main": "dist/jquery.js"
-			}
-		}
-	}))
+	return gulp.src(mainBowerFiles())
 		.pipe(gulp.dest('./.tmp/vendors'))
 });
 // 將 Bower 的 .js 檔案合併成一個
-gulp.task('vendorJS', ['bower'], function(){
-	return gulp.src('./.tmp/vendors/**/*.js')
+gulp.task('vendorJS', function(){
+	return gulp.src([
+		'./.tmp/vendors/**/*.js',
+		'./node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+		])
 		.pipe($.order([
 			'jquery.js',
-			'bootstrap.bundle.js'
+			'bootstrap.js'
 		]))
 		.pipe($.concat('vendors.js'))
 		.pipe($.if(options.env === 'production', $.uglify()))
@@ -92,7 +89,7 @@ gulp.task('vendorJS', ['bower'], function(){
 
 // 圖片壓縮
 gulp.task('image-min', function(){
-	gulp.src('./source/images/*')
+	return gulp.src('./source/images/*')
 		.pipe($.if(options.env === 'productino', $.imagemin()))
 		.pipe(gulp.dest('./public/images'))
 })
@@ -104,26 +101,62 @@ gulp.task('deploy', function () {
 		.pipe($.ghPages());
 });
 
-// 虛擬伺服器開在指定路徑下
-gulp.task('bower-sync', function(){
-	browserSync.init({
-		server: {
-			baseDir: "./public"
-		}
-	})
-})
+// gulp 3.x 版虛擬伺服器開在指定路徑下
+// gulp.task('bower-sync', function(){
+// 	browserSync.init({
+// 		server: {
+// 			baseDir: "./public"
+// 		},
+// 		reloadDebounce: 2000
+// 	})
+// })
 
-// 監控指定資料夾 指令：gulp watch
+// gulp 3.x 版監控指定資料夾 指令：gulp watch
 // 無法監控到『新增』及『刪除』的檔案更動
-gulp.task('watch', function () {
-	gulp.watch('./source/**/*.html', ['copyHTML']);
-	gulp.watch('./source/scss/**/*.scss', ['sass']);
-	gulp.watch('./source/js/**/*.js', ['babel']);
-});
+// gulp.task('watch', function () {
+// 	gulp.watch('./source/**/*.html', ['copyHTML']);
+// 	gulp.watch('./source/scss/**/*.scss', ['sass']);
+// 	gulp.watch('./source/js/**/*.js', ['babel']);
+// });
 
-// 一次執行所有『正式用』任務流程 指令：gulp sequence
-gulp.task('build', $.sequence('clean', 'copyHTML', 'sass', 'babel', 'vendorJS', 'image-min'));
+// gulp 3.x 版一次執行所有『正式用』任務流程 指令：gulp sequence
+// gulp.task('build', $.sequence('clean', 'copyHTML', 'sass', 'babel', 'vendorJS', 'image-min'));
 
-// 一次執行所有『開發用』任務流程 指令：gulp
-gulp.task('default', ['copyHTML', 'sass', 'babel', 'vendorJS', 'image-min', 'bower-sync', 'watch']);
+// gulp 3.x 版一次執行所有『開發用』任務流程 指令：gulp
+// gulp.task('default', ['copyHTML', 'sass', 'babel', 'vendorJS', 'image-min', 'bower-sync', 'watch']);
 
+// gulp 4.0 指令 (parallel / series)
+// parallel 同時執行
+// series 依序執行
+
+gulp.task('build'
+	,gulp.series(
+		'clean',
+		'bower',
+		'vendorJS',
+		gulp.parallel('copyHTML', 'sass', 'babel', 'image-min')
+	)
+)
+
+gulp.task('default'
+	, gulp.series(
+		'clean',
+		'bower',
+		'vendorJS',
+		gulp.parallel('copyHTML', 'sass', 'babel', 'image-min'),
+		function(done){
+			// gulp 4.0 browserSync 指令寫法
+			browserSync.init({
+				server: {
+					baseDir: "./public"
+				},
+				reloadDebounce: 2000
+			})
+			// gulp 4.0 watch 指令寫法
+			gulp.watch(['./source/**/*.html'], gulp.series('copyHTML'));
+			gulp.watch(['./source/scss/**/*.scss'], gulp.series('sass'));
+			gulp.watch(['./source/js/**/*.js'], gulp.series('babel'));
+			done();
+		}
+	)
+)
